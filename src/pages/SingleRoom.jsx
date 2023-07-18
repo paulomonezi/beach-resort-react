48f41f327cf404017d6978f6e647cb85a11851f3
Paulo Monezi <paulo.monezi@icloud.com>
2023-07-18 09:33:35 -0400

    refactor(singleroom): changed class component to functional component to work  with useParams()

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,55 +1,45 @@
-import React, { Component } from "react"
-import defaultBcg from "../images/room-1.jpeg"
-import Hero from "../components/Hero"
-import Banner from "../components/Banner"
-import { Link } from "react-router-dom"
-import { RoomContext } from "../context"
+import React from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link, useParams } from "react-router-dom";
+import { RoomContext } from "../context";
 
-export default class SingleRoom extends Component {
-  constructor(props) {
-    super(props)
-    console.log(this.props)
-    this.state = {
-      slug: this.props.match.params.slug,
-      defaultBcg,
-    }
+export default function SingleRoom() {
+  const { slug } = useParams();
+  const { getRoom } = React.useContext(RoomContext);
+  const room = getRoom(slug);
+
+  if (!room) {
+    return (
+      <div className="error">
+        <h3>no such room could be found</h3>
+        <Link to="/rooms" className="btn-primary">
+          back to rooms
+        </Link>
+      </div>
+    );
   }
 
-  static contextType = RoomContext
-  // componentDidMount() {}
+  const {
+    name,
+    description,
+    capacity,
+    size,
+    price,
+    extras,
+    breakfast,
+    pets,
+    images,
+  } = room;
 
-  render() {
-    const { getRoom } = this.context
-    const room = getRoom(this.state.slug)
-    if (!room) {
-      return (
-        <div className="error">
-          <h3>no such room could be found</h3>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </div>
-      )
-    }
-    const {
-      name,
-      description,
-      capacity,
-      size,
-      price,
-      extras,
-      breakfast,
-      pets,
-      images,
-    } = room
-
-    console.log(room)
-    return <Hero hero='roomsHero'>
+  return (
+    <Hero hero="roomsHero">
       <Banner title={`${name} room`}>
-        <Link to='/rooms' className="btn-primary">
+        <Link to="/rooms" className="btn-primary">
           back to rooms
         </Link>
       </Banner>
     </Hero>
-  }
+  );
 }
