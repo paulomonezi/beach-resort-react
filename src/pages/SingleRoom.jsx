ff267905fccf236f6360802ba886566fc98cfbb4
Paulo Monezi <paulo.monezi@icloud.com>
2023-07-24 16:21:08 -0400

    refactor(single room): refactor single room component using styled-components

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import defaultBcg from "../images/room-1.jpeg";
-import Hero from "../components/Hero";
-import Banner from "../components/Banner";
-import { Link, useParams } from "react-router-dom";
-import { RoomContext } from "../context";
+import React from "react"
+import defaultBcg from "../images/room-1.jpeg"
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import { Link, useParams } from "react-router-dom"
+import { RoomContext } from "../context"
+import StyledHero from "../components/StyledHero"
 
 export default function SingleRoom() {
-  const { slug } = useParams();
-  const { getRoom } = React.useContext(RoomContext);
-  const room = getRoom(slug);
+  const { slug } = useParams()
+  const { getRoom } = React.useContext(RoomContext)
+  const room = getRoom(slug)
 
   if (!room) {
     return (
@@ -18,7 +19,7 @@ export default function SingleRoom() {
           back to rooms
         </Link>
       </div>
-    );
+    )
   }
 
   const {
@@ -31,15 +32,53 @@ export default function SingleRoom() {
     breakfast,
     pets,
     images,
-  } = room;
+  } = room
 
+  // array destructuring for rendering all images but the main/cover image
+  const [mainImg, ...defaultImg] = images
   return (
-    <Hero hero="roomsHero">
-      <Banner title={`${name} room`}>
-        <Link to="/rooms" className="btn-primary">
-          back to rooms
-        </Link>
-      </Banner>
-    </Hero>
-  );
+    <>
+      <StyledHero img={mainImg}>
+        {/* <Hero hero="roomsHero"> */}
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+        {/* </Hero> */}
+      </StyledHero>
+      <section className="single-room">
+        <div className="single-room-images">
+          {defaultImg.map((item, index) => {
+            return <img key={index} src={item} alt={name} />
+          })}
+        </div>
+        <div className="single-room-info">
+          <article className="desc">
+            <h3>details</h3>
+            <p>{description}</p>
+          </article>
+          <article className="info">
+            <h3>info</h3>
+            <h6>price: ${price}</h6>
+            <h6>size: {size} sqft</h6>
+            <h6>
+              max capacity :{" "}
+              {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+            </h6>
+            <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+            <h6>{breakfast && "free breakfast included"}</h6>
+          </article>
+        </div>
+      </section>
+      <section className="room-extras">
+        <h6>extras</h6>
+        <ul className="extras">
+          {extras.map((item, index) => {
+            return <li key={index}>- {item}</li>
+          })}
+        </ul>
+      </section>
+    </>
+  )
 }
