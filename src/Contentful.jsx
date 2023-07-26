d57f999bf41babdb70963da634bad3ce97839d47
Paulo Monezi <paulo.monezi@icloud.com>
2023-07-26 16:58:55 -0400

    chore(contentful): api keys settings

--- src/Contentful.jsx
+++ src/Contentful.jsx
@@ -1,8 +1,8 @@
-import { createClient } from "contentful";
-import { API_ACCESS_TOKEN, API_KEY_SPACE } from "../env.development";
-
+import { createClient } from "contentful"
+const API_KEY_SPACE = process.env.REACT_APP_API_KEY_SPACE
+const API_TOKEN = process.env.REACT_APP_API_ACCESS_TOKEN
 
 export default createClient({
-    space: API_KEY_SPACE,
-    accessToken: API_ACCESS_TOKEN
+  space: API_KEY_SPACE,
+  accessToken: API_TOKEN,
 })
