87fd8e5589e77e6fa52e35bab2538a19f78eaa9b
Paulo Monezi <paulo.monezi@icloud.com>
2023-07-26 17:13:36 -0400

    fix(contentful): contentful fix

--- src/Contentful.jsx
+++ src/Contentful.jsx
@@ -1,8 +1,6 @@
 import { createClient } from "contentful"
-const API_KEY_SPACE = process.env.REACT_APP_API_KEY_SPACE
-const API_TOKEN = process.env.REACT_APP_API_ACCESS_TOKEN
 
 export default createClient({
-  space: API_KEY_SPACE,
-  accessToken: API_TOKEN,
+  space: '9fpl7x1g9uro',
+  accessToken: 'ADfEEqTIKaxFmR6wpyawIQJpH6H-X7bRhgr-ckHNPoQ'
 })
